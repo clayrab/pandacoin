db537b481de16372192fe312fcca60ab971d9f4f
clayrab <claytonrabenda@gmail.com>
2022-01-25 02:11:00 +0000

    Deploying to gh-pages from @ clayrab/pandacoin@a66bf157050489d3a2c928f6be19062f2d1c3c4c 🚀

--- pandacoin/types/sidebar-items.js
+++ pandacoin/types/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"type":[["PandaAddress","The proto library will not decode to an array, only a vector. This “type” only exists solely for the sake of semantics, so that it’s clear what sort of data the Vec is carrying"],["Secp256k1SignatureCompact",""],["Sha256Hash","Sha256Hash byte array type"]]});
+initSidebarItems({"type":[["PandaAddress","The proto library will not decode to an array, only a vector. This “type” only exists solely for the sake of semantics, so that it’s clear what sort of data the Vec is carrying"],["Secp256k1SignatureCompact",""],["Sha256Hash",""]]});
